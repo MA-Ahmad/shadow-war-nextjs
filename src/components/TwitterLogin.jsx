70d81124f5d20e32b9094c22a45e36a7debd0bfb
Muhammad Ahmad <muhammad.ahmad8043@gmail.com>
2023-08-15 01:40:56 +0500

    change

--- src/components/TwitterLogin.jsx
+++ src/components/TwitterLogin.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import { Button, Flex } from '@chakra-ui/react'
 import { FaTwitter } from 'react-icons/fa'
 
-const BASE_URL = 'http://localhost:3001'
+const BASE_URL = 'https://shadow-war-mission-backend-3a11b40a384a.herokuapp.com'
 
 const TwitterLoginButton = () => {
   const handleTwitterLogin = () => {
